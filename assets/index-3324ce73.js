a234770d6dff9db0fb8fb08fdd7a9eada81dbfa2
santoshkurmi <xantosh121@gmail.com>
2023-05-24 16:25:38 +0000

    Update from https://github.com/Santoshkurmi/metis_website/commit/9ce252957090ed76fbf180f907ff0e64746ebeb1

---
+++ assets/index-3324ce73.js
@@ -0,0 +1,1 @@
+import{b as e,H as o}from"./entry-client-1088d4ba.js";function a(){return e(o,{})}export{a as default};
--- assets/keybindings-d34f2404.js
+++ assets/keybindings-d34f2404.js
@@ -1,4 +1,4 @@
-import{M as r}from"./index-6e722fd8.js";import{b as n,q as o}from"./entry-client-48be99b3.js";function i(t){const e=Object.assign({h1:"h1",p:"p",ul:"ul",li:"li",h2:"h2",a:"a"},r(),t.components);return[n(e.h1,{children:"KeyMapping"}),`
+import{M as r}from"./index-16fc2f59.js";import{b as n,q as o}from"./entry-client-1088d4ba.js";function i(t){const e=Object.assign({h1:"h1",p:"p",ul:"ul",li:"li",h2:"h2",a:"a"},r(),t.components);return[n(e.h1,{children:"KeyMapping"}),`
 `,n(e.p,{children:"The key bindings are defined in the window manager itself (dwm) which is configured in its config file (config.h)."}),`
 `,n(e.ul,{get children(){return[`
 `,n(e.li,{children:"Note: MOD is super (windows) key"}),`
