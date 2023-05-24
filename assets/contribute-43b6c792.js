d7992a7419235cea4f9bdd8f7effa00a1acb8324
santoshkurmi <xantosh121@gmail.com>
2023-05-24 16:19:35 +0000

    Update from https://github.com/Santoshkurmi/metis_website/commit/75b5e88cf4b366e05ecfd1658e23cdb63fe7fdd9

--- assets/contribute-43b6c792.js
+++ assets/contribute-43b6c792.js
@@ -1,4 +1,4 @@
-import{M as h}from"./index-854bc80d.js";import{b as n,q as i}from"./entry-client-7d928f09.js";function t(r){const e=Object.assign({h1:"h1",p:"p",h3:"h3",ul:"ul",li:"li",a:"a"},h(),r.components);return[n(e.h1,{children:"Metis Linux Thanks Page"}),`
+import{M as h}from"./index-6e722fd8.js";import{b as n,q as i}from"./entry-client-48be99b3.js";function t(r){const e=Object.assign({h1:"h1",p:"p",h3:"h3",ul:"ul",li:"li",a:"a"},h(),r.components);return[n(e.h1,{children:"Metis Linux Thanks Page"}),`
 `,n(e.p,{children:"Huge thanks and respect to each and every helping hands who made possible to create metis-os !!"}),`
 `,n(e.h3,{children:"Core Team"}),`
 `,n(e.ul,{get children(){return[`
