3e02cc88a29893b86de3512980b54f5447c31d94
santoshkurmi <xantosh121@gmail.com>
2023-05-24 14:56:27 +0000

    Update from https://github.com/Santoshkurmi/metis_website/commit/04cdcf3835b872451f3cb2d928b59c3d606f9b2e

---
+++ assets/install-2dd68686.js
@@ -0,0 +1,54 @@
+import{b as n,q as o,g as l,t as s}from"./entry-client-485cbedf.js";import{M as r}from"./index-b2a4e08e.js";const a=s('<iframe width="560" height="315" src="https://www.youtube.com/embed/3nPmJmD8FX8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',2);function i(t){const e=Object.assign({h1:"h1",p:"p",ul:"ul",li:"li",a:"a",pre:"pre",code:"code",span:"span",img:"img"},r(),t.components);return[n(e.h1,{children:"Pre-installation"}),`
+`,n(e.p,{children:"Note: Metis Linux installation images do not support Secure Boot. You will need to disable Secure Boot to boot the installation medium."}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{get children(){return["Download Metis Linux from our ",n(e.a,{href:"https://objects.githubusercontent.com/github-production-release-asset-2e65be/512371970/0cf75ce8-3b1c-4274-886b-e9c2a67a7dce?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230409%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230409T143609Z&X-Amz-Expires=300&X-Amz-Signature=65c337df2d16fe942dafd4b24d8023ebb91a5190e2a0f4471997b8c012f659f2&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=512371970&response-content-disposition=attachment%3B%20filename%3Dmetis-base-runit-20220717-v1.1.1-x86_64.iso&response-content-type=application%2Foctet-stream",children:"Download Page"})]}}),`
+`]}}),`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"Flash the Metis OS image in a usb stick. Plug it in your machine and live boot from the USB stick."}),`
+`]}}),`
+`]}}),`
+`,n(e.h1,{children:"Preparation For Installation"}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"Verify the boot mode To verify the boot mode uefi or legacy, list the efivars directory:"}),`
+`]}}),`
+`,n(e.pre,{get children(){return n(e.code,{className:"hljs language-bash",get children(){return[" ",n(e.span,{className:"hljs-built_in",children:"ls"}),` /sys/firmware/efi/efivars
+`]}})}}),`
+`,n(e.p,{children:"If the command shows the directory without error, then the system is booted in UEFI mode. If the directory does not exist, the system may be booted in BIOS (or CSM) mode. If the system did not boot in the mode you desired, refer to your motherboard's manual."}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"Note1: For UEFI Systems, you must create a boot partition of at least 300 MBs (512 MBs recommended) for grub installation during the installation process. Skip for legacy systems."}),`
+`]}}),`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"Note2: Legacy installation only supports limited filesystem types like ext4, xfs. It doesnot support btrfs file system."}),`
+`]}}),`
+`]}}),`
+`,n(e.h1,{children:"Connect to the internet"}),`
+`,n(e.p,{children:"To set up a network connection in the live environment, go through the following steps:"}),`
+`,n(e.pre,{get children(){return n(e.code,{className:"hljs language-bash",get children(){return["ip ",n(e.span,{className:"hljs-built_in",children:"link"}),` 
+`]}})}}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"For wireless and WWAN, make sure the card is not blocked with rfkill."}),`
+`,n(e.li,{get children(){return["Connect to the network:",`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"Ethernet—plug in the cable."}),`
+`,n(e.li,{children:`You can either use nmcli or nmtui to connect to the wifi networks.
+The connection may be verified with ping:`}),`
+`]}}),`
+`]}}),`
+`]}}),`
+`,n(e.pre,{get children(){return n(e.code,{className:"hljs language-bash",children:`ping -c 3 metislinux.org
+`})}}),`
+`,n(e.h1,{children:"Installation Process Using metis-sideload"}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"Execute the command metis-sideload and follow the instructions:"}),`
+`]}}),`
+`,n(e.pre,{get children(){return n(e.code,{className:"hljs language-bash",children:`metis-sideload 
+`})}}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"Login after successful installation"}),`
+`,n(e.li,{get children(){return[`After completing installation, select xinitrc in the login screen and then enter your username and password to login.
+`,n(e.img,{src:"https://metislinux.org/assets/images/pages/installation-guide/metis-login-screen.png",alt:"login_screen"})]}}),`
+`]}}),`
+`,n(e.h1,{children:"Here is a installation tutorial video made by one of the metis linux user!"}),`
+`,l(a)]}function h(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n(e,o(t,{get children(){return n(i,t)}})):i(t)}export{h as default};
---
+++ assets/post-install-f3ba2251.js
@@ -0,0 +1,30 @@
+import{b as n,q as o,g as l,t as s}from"./entry-client-485cbedf.js";import{M as i}from"./index-b2a4e08e.js";const a=s('<button style="background:green;"><a style="color:white;text-decoration:none" href="https://github.com/metis-os/metis-iso/releases/download/v1.1.1/metis-base-runit-20220717-v1.1.1-x86_64.iso">Download Now</a></button>',4);function r(t){const e=Object.assign({h1:"h1",ul:"ul",li:"li",p:"p",a:"a"},i(),t.components);return[n(e.h1,{children:"Download Metis Linux"}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"Download Metis Linux and replace your current bloated resorce hungry operating system whether it's Windows or macOS or run Metis Linux alongside it."}),`
+`]}}),`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"Ready To Download?"}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{children:"Get the systemd-less, super smooth, ultra minimalistic, resource efficient, highly customizable and absolute performance oriented operating system for super productivity."}),`
+`]}}),`
+`]}}),`
+`]}}),`
+`,l(a),`
+`,n(e.h1,{children:"Alternative Download Links!"}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"Download from github releases"}),`
+`,n(e.ul,{get children(){return[`
+`,n(e.li,{get children(){return n(e.a,{href:"https://github.com/metis-os/metis-iso/releases",children:"https://github.com/metis-os/metis-iso/releases"})}}),`
+`]}}),`
+`]}}),`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"Check our installation guide for detailed installation instructions."}),`
+`]}}),`
+`,n(e.li,{get children(){return[`
+`,n(e.p,{children:"note: You may not get full performance if you're installing metis linux in a virtual machine!"}),`
+`]}}),`
+`]}}),`
+`,n(e.p,{children:`Yes! Metis Linux is Completely Free and open source. Want to check it's source code?
+Follow us on GitHub.`})]}function d(t={}){const{wrapper:e}=Object.assign({},i(),t.components);return e?n(e,o(t,{get children(){return n(r,t)}})):r(t)}export{d as default};
