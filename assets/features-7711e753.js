1cd982d804372e046a5cd47858fff11fca830d15
santoshkurmi <xantosh121@gmail.com>
2023-05-24 16:12:47 +0000

    Update from https://github.com/Santoshkurmi/metis_website/commit/be97e26b13d70075e3d98d5db3c8395e3668e649

--- assets/features-7711e753.js
+++
@@ -1,1 +0,0 @@
-import{M as r}from"./index-33f5961f.js";import{b as t,q as c}from"./entry-client-3ef93302.js";function o(e){const n=Object.assign({h2:"h2"},r(),e.components);return t(n.h2,{children:"Here features of metisLinux will be shown"})}function i(e={}){const{wrapper:n}=Object.assign({},r(),e.components);return n?t(n,c(e,{get children(){return t(o,e)}})):o(e)}export{i as default};
--- assets/keybindings-7ff0b38a.js
+++ assets/keybindings-7ff0b38a.js
@@ -1,4 +1,4 @@
-import{M as r}from"./index-33f5961f.js";import{b as n,q as o}from"./entry-client-3ef93302.js";function i(t){const e=Object.assign({h1:"h1",p:"p",ul:"ul",li:"li",h2:"h2",a:"a"},r(),t.components);return[n(e.h1,{children:"KeyMapping"}),`
+import{M as r}from"./index-854bc80d.js";import{b as n,q as o}from"./entry-client-7d928f09.js";function i(t){const e=Object.assign({h1:"h1",p:"p",ul:"ul",li:"li",h2:"h2",a:"a"},r(),t.components);return[n(e.h1,{children:"KeyMapping"}),`
 `,n(e.p,{children:"The key bindings are defined in the window manager itself (dwm) which is configured in its config file (config.h)."}),`
 `,n(e.ul,{get children(){return[`
 `,n(e.li,{children:"Note: MOD is super (windows) key"}),`
