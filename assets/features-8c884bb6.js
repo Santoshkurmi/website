3840c3ded511ba8ae3fd6d01c062455fb69ca859
santoshkurmi <xantosh121@gmail.com>
2023-06-11 07:22:15 +0000

    Update from https://github.com/Santoshkurmi/metis_website/commit/e3e75d52e28a716e82b88a9d64ee50c33680e513

---
+++ assets/features-8c884bb6.js
@@ -0,0 +1,1 @@
+import{M as r}from"./index-441305c2.js";import{b as t,v as c}from"./entry-client-82307c61.js";function o(e){const n=Object.assign({h2:"h2"},r(),e.components);return t(n.h2,{children:"Here features of metisLinux will be shown"})}function i(e={}){const{wrapper:n}=Object.assign({},r(),e.components);return n?t(n,c(e,{get children(){return t(o,e)}})):o(e)}export{i as default};
